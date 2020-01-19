dda27a470c3e9b8812ca6a456bebb38629408659
lobana alarabi <loubanaeng@gmail.com>
2020-01-19 17:06:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
 const companyName = 'Scuber';
+companyName='lara';
 let mostProfitableNeighborhood='Chelsea';
 let companyCeo='Susan Smith';
