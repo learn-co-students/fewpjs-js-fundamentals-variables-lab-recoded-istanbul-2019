5a018944a6544c7096f4a7cdcadf293caa96b21a
Halit Batur <halitfuatbatur@gmail.com>
2020-04-03 14:54:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+companyCeo = 'Lauren Hart';
+mostProfitableNeighborhood = 'Upper West Side';
