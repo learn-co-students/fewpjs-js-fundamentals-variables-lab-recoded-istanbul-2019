cce3cb08d2aa2c0fa9c0fe3a65f239abe3de2a0b
aydin <aydindavut14@gmail.com>
2020-04-04 11:46:13 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+describe("'index.js", function () {
+    it("is set as Scuber", function () {
+        expect(companyName).to.equal('Scuber')
+    })
+
+})
+
+mostProfitableNeighborhood = "Upper West Side";
